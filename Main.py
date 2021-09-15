5e4c9edf675a5a08bcce3bd8c5d01f01bff616aa
umairtehami <46089239+umairtehami@users.noreply.github.com>
2021-09-15 16:07:24 +0200

    Delete Main.py

--- Main.py
+++
@@ -1,9 +0,0 @@
-from Main_Window import *
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Widget = QtWidgets.QStackedWidget()
-    MainWindow = MainWindow()
-    MainWindow.show()
-    sys.exit(app.exec_())
