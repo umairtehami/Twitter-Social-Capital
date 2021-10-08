7037e711fd9e5544b5369483194baef5534d27c3
umairtehami <46089239+umairtehami@users.noreply.github.com>
2021-10-08 08:04:19 +0200

    Delete Source Code directory

--- Source Code/Authentication.py
+++
@@ -1,7 +0,0 @@
-
-# Saves the name and the path of the credentials.
-class Authentication:
-
-    def __init__(self, name, path):
-        self.name = name
-        self.path = path
--- Source Code/Extraction.py
+++
@@ -1,8 +0,0 @@
-
-#Extraction is a gerarquical class, Unweighted, Weighted and Tweets classes depends on it.
-class Extraction:
-
-    def __init__(self, list, delay = 0):
-        self.list = list
-        self.delay = delay
-
